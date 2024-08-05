f5a555051301ea2f335669d6d66da7447f8d5769
Kauã Guimarães <162592112+KauaSg@users.noreply.github.com>
2024-08-04 23:37:01 -0300

    Add files via upload

---
+++ Lógica de programação praticando com desafios/amigo-secreto/js/app.js
@@ -0,0 +1,62 @@
+let listaNomesDigitados = []
+
+function adicionar() {
+    nomeDigitado = document.getElementById("nome-amigo").value
+
+    if (listaNomesDigitados.includes(nomeDigitado)){
+        alert("Contém dois nomes iguais, por favor, insira um sobrenome para diferenciar.");
+        return false;
+    }
+
+    if (nomeDigitado === "") {
+        alert("Por favor, insira um nome válido.");
+        return false;
+    }
+
+    listaNomesDigitados.push(nomeDigitado)
+   
+    document.getElementById('lista-amigos').textContent = listaNomesDigitados.join(", ");
+
+    document.getElementById("nome-amigo").value = ''
+
+    console.log(listaNomesDigitados)
+}
+
+function embaralhar(lista) {
+    for (let i = lista.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [lista[i], lista[j]] = [lista[j], lista[i]];
+    }
+    return lista;
+}
+
+function sortear() {
+    if(listaNomesDigitados.length < 4){
+        alert("Por favor, insira pelo menos quatro nomes.");
+        return false;
+    }
+    embaralhar(listaNomesDigitados);
+    let resultado = "";
+    for (let i = 0; i < listaNomesDigitados.length; i++) {
+        if(i === listaNomesDigitados.length - 1){
+            resultado += `${listaNomesDigitados[i]} --> ${listaNomesDigitados[0]}<br>`;
+        } else {
+            resultado += `${listaNomesDigitados[i]} --> ${listaNomesDigitados[i+1]}<br>`;
+    }
+    document.getElementById('lista-sorteio').innerHTML = resultado;
+}
+}
+
+function reiniciar() {
+    document.getElementById('lista-sorteio').innerHTML = ''
+    document.getElementById('lista-amigos').textContent = ''
+    listaNomesDigitados = []
+
+}
+
+
+
+
+
+
+
