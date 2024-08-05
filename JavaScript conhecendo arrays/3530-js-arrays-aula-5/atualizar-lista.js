0c367eba503a249f8ed52dc7cf4776d3f7e1d624
Kauã Guimarães <162592112+KauaSg@users.noreply.github.com>
2024-08-04 23:41:47 -0300

    Add files via upload

---
+++ JavaScript conhecendo arrays/3530-js-arrays-aula-5/atualizar-lista.js
@@ -0,0 +1,8 @@
+const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+
+listaEstudantes.splice(1, 2, 'Rodrigo');
+// listaEstudantes.push('Rodrigo');
+
+console.log(listaEstudantes);
+
+// adiciona ou remove itens splice(1, 2, 'Rodrigo')   1 = indice, 2 = itens excluidos, Oque deseja incluir ou colocar no lugar
